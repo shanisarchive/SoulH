2a46b8df743455c3695651dd56858e27ed8e8d11
shanisarchive <shanisarchive@gmail.com>
2024-11-13 17:58:07 +0530

    Update nlp.js

--- src/services/nlp.js
+++ src/services/nlp.js
@@ -1,1 +1,15 @@
+import * as tf from '@tensorflow/tfjs';
+
+export const classifyReport = async (text) => {
+  const input = tokenizer.encode(text);
+  const prediction = model.predict(tf.tensor([input]));
+  return prediction > 0.5 ? 'Medical Report' : 'Not a Medical Report';
+};
+
+export const extractEntities = async (text) => {
+  const conditions = text.match(/(diabetes|hypertension|etc)/gi);
+  const vitalSigns = text.match(/(blood pressure|heart rate|glucose)/gi);
+
+  return { conditions, vitalSigns };
+};
 
